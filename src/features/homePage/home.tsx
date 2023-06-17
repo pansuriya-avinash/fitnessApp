5cfb46edc97ab3bf9a126d46dde01cef14553331
avinash pansuriya <avinash.pansuriya@scaletech.xyz>
2023-06-17 18:28:14 +0530

    user all activity page design

--- src/features/homePage/home.tsx
+++ src/features/homePage/home.tsx
@@ -1,13 +1,29 @@
 import React, { useState } from "react";
 import { userData } from "./util/utils";
+import { Pie } from "react-chartjs-2";
+import moment from "moment";
 
 const home = () => {
-	const [userActivity,setUserActivity]=useState<any>()
+  const [userActivity, setUserActivity] = useState<any>();
 
   const handelActivity = (id: string) => {
     const activity = userData.activities.find((e) => e.id == id);
-    
-	setUserActivity(activity)
+
+    setUserActivity(activity);
+  };
+  const data = {
+    labels: ["Red", "Blue"],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: userActivity && [
+          userActivity.heart_rate.max,
+          userActivity.heart_rate.average,
+        ],
+        backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)"],
+        hoverOffset: 4,
+      },
+    ],
   };
   return (
     <div className="container-dashboard">
@@ -30,21 +46,32 @@ const home = () => {
         ))}
       </div>
 
-      {userActivity&&
-		<div className="dashboard">
-          <div
-            className="activity-wrapper user-activity"
-          >
-            <div
-              dangerouslySetInnerHTML={{ __html: userActivity.icon }}
-              className="activity-icon"
-            />
-         
-            <h6 className="activity-name">{userActivity.name}</h6>
-          </div>
-       
+      {userActivity && (
+        <div>
+          <div className="dashboard">
+            <div className="activity-wrapper user-activity">
+              <div
+                dangerouslySetInnerHTML={{ __html: userActivity.icon }}
+                className="activity-icon"
+              />
 
-	  </div>}
+              <h6 className="activity-name">{userActivity.name}</h6>
+            </div>
+          </div>
+          <div className="dashboard " >
+			<div className="activity-wrapper ">
+            <Pie data={data} />
+			<h6 className="activity-name text-align-center mt-3">Heart Rate</h6>
+			</div>
+          </div>
+		  <div className="dashboard " >
+			<div className="activity-wrapper ">
+			<p  className="activity-name text-align-center ">Activity Date </p>
+            <h4 className="text-align-center">{moment(userActivity.date).format(' MMMM DD YYYY')}</h4>
+			</div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
