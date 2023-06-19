2df7b131b55d7f36baba6ee5190d5b133e343b3e
avinash pansuriya <avinash.pansuriya@scaletech.xyz>
2023-06-19 10:45:47 +0530

    activity details show layout

--- src/features/homePage/home.tsx
+++ src/features/homePage/home.tsx
@@ -2,14 +2,17 @@ import React, { useState } from "react";
 import { userData } from "./util/utils";
 import { Pie } from "react-chartjs-2";
 import moment from "moment";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRectangleXmark } from "@fortawesome/free-solid-svg-icons";
 
 const home = () => {
   const [userActivity, setUserActivity] = useState<any>();
+  const [activitiesDetails, setActivitiesDetails] = useState(false);
 
   const handelActivity = (id: string) => {
     const activity = userData.activities.find((e) => e.id == id);
-
     setUserActivity(activity);
+    setActivitiesDetails(true);
   };
   const data = {
     labels: ["Red", "Blue"],
@@ -27,49 +30,78 @@ const home = () => {
   };
   return (
     <div className="container-dashboard">
-      <div className="dashboard">
-        {userData.activities.map((value, i) => (
-          <div
-            key={i}
-            className="activity-wrapper"
-            onClick={() => handelActivity(value.id)}
-          >
+      {!activitiesDetails && (
+        <div className="dashboard">
+          {userData.activities.map((value, i) => (
             <div
-              dangerouslySetInnerHTML={{ __html: value.icon }}
-              className="activity-icon"
-            />
-            <p className="activity-time">
-              Duration: <span>{value.duration}</span>
-            </p>
-            <h6 className="activity-name">{value.name}</h6>
-          </div>
-        ))}
-      </div>
-
-      {userActivity && (
-        <div>
-          <div className="dashboard">
-            <div className="activity-wrapper user-activity">
+              key={i}
+              className="activity-wrapper"
+              onClick={() => handelActivity(value.id)}
+            >
               <div
-                dangerouslySetInnerHTML={{ __html: userActivity.icon }}
+                dangerouslySetInnerHTML={{ __html: value.icon }}
                 className="activity-icon"
               />
-
-              <h6 className="activity-name">{userActivity.name}</h6>
+              <p className="activity-time">
+                Duration: <span>{value.duration}</span>
+              </p>
+              <h6 className="activity-name">{value.name}</h6>
             </div>
+          ))}
+        </div>
+      )}
+
+      {activitiesDetails && (
+        <div>
+        <div className="text-align-right mb-3 ">
+          <FontAwesomeIcon icon={faRectangleXmark} onClick={()=>setActivitiesDetails(false)} className="close-icone"/>
+        </div>
+        <div className="dashboard">
+          <div className="activity-wrapper user-activity">
+            <div
+              dangerouslySetInnerHTML={{ __html: userActivity.icon }}
+              className="activity-icon"
+            />
+
+            <h6 className="activity-name">{userActivity.name}</h6>
           </div>
-          <div className="dashboard " >
-			<div className="activity-wrapper ">
+
+          <div className="activity-wrapper user-activity">
+            <p className="activity-name text-align-center ">Activity Date </p>
+            <h4 className="text-align-center">
+              {moment(userActivity.date).format(" MMMM DD YYYY")}
+            </h4>
+          </div>
+
+          <div className="activity-wrapper user-activity">
             <Pie data={data} />
-			<h6 className="activity-name text-align-center mt-3">Heart Rate</h6>
-			</div>
+            <h6 className="activity-name text-align-center mt-3">Heart Rate</h6>
           </div>
-		  <div className="dashboard " >
-			<div className="activity-wrapper ">
-			<p  className="activity-name text-align-center ">Activity Date </p>
-            <h4 className="text-align-center">{moment(userActivity.date).format(' MMMM DD YYYY')}</h4>
-			</div>
+
+          <div className="activity-wrapper user-activity">
+            <p className="activity-name text-align-center ">
+              {" "}
+              Calories Burned{" "}
+            </p>
+            <h4 className="text-align-center">
+              {userActivity.calories_burned}
+            </h4>
           </div>
+
+          <div className="activity-wrapper user-activity">
+            <p className="activity-name text-align-center "> Distance </p>
+            <h4 className="text-align-center">
+              {userActivity.distance || "-"}
+            </h4>
+          </div>
+
+          <div className="activity-wrapper user-activity">
+            <p className="activity-name text-align-center "> Duration </p>
+            <h4 className="text-align-center">
+              {userActivity.duration || "-"}
+            </h4>
+          </div>
+        </div>
         </div>
       )}
     </div>
